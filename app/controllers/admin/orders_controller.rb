27421da5af36470588180cf4b84a4b44edf94c5e
EC2 Default User <ec2-user@ip-172-31-11-38.ap-northeast-1.compute.internal>
2023-08-19 08:58:41 +0000

    [add]注文情報作成中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,11 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_member!
+  
+  def new
+    @order = Order.new
+    @addresses = current_customer.addresses.a
+  end
+  
   def show
   end
 end
