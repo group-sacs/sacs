9136d61ee491ffa9f28a90ca168bdf073f4de6d3
chimu-a <138343606+chimu-a@users.noreply.github.com>
2023-08-20 05:49:17 +0000

    [add]マイページ会員情報編集退会確認編集中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,31 @@
 class Public::CustomersController < ApplicationController
   def show
+　　@customer = Customer.find(params[:id])
   end
 
   def edit
+　　@customer = Customer.find(params[:id])
   end
 
-  def confirm
+  def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to current_customer_path(customer.id)
   end
+
+
+  def confirm
+    @customer = Customer.find(params[:id])
+  end
+  
+  def withdra
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
