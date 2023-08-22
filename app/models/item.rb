99bd0ef503473f7531f965d2505a91c36d66bfc1
chimu-a <138343606+chimu-a@users.noreply.github.com>
2023-08-22 06:39:01 +0000

    [add]商品一覧　途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,14 @@ class Item < ApplicationRecord
  has_many :cart_items
   with_options presence: true do
    validates :name
-   validates :description
-   validates :price
-   validates :stock
+   validates :explamatory_text
+   validates :without_tax_price
    validates :image
  end
  has_one_attached :image
+# 下記追記した
+ def add_tax_without_tax_price
+  (self.without_tax_price * 1.10).round
+ end
+
 end
