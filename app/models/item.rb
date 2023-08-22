931721f2e1f583ccdaff781578b9e31a68e6833f
Raichi Ando <135943914+salty-light@users.noreply.github.com>
2023-08-22 07:03:47 +0000

    mikansei

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,10 @@ class Item < ApplicationRecord
  has_many :cart_items
   with_options presence: true do
    validates :name
-   validates :description
-   validates :price
-   validates :stock
+   validates :explamatory_text
+   validates :without_tax_price
    validates :image
  end
+
  has_one_attached :image
 end
