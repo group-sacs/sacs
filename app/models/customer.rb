d8fba615019f4eb18e58be4227e29ac95f586657
RyujiShimatani <138374692+RyujiShimatani@users.noreply.github.com>
2023-08-23 07:25:20 +0000

    レイアウト修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,5 +6,20 @@ class Customer < ApplicationRecord
 
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  has_many :delivery_addresses
+  has_many :delivery_addresses, dependent: :destroy
+
+  validates :last_name, presence:true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true, length: {maximum: 7, minimum: 7}, numericality: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true, length: {maximum: 11, minimum: 10}, numericality: true
+  validates :email, uniqueness: true
+
+# ユーザーアカウントが認証可能か確認コード
+  # def active_for_authentication?
+  #   super && (self.is_deleted == false)
+  # end
+  
 end
