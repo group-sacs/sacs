7e4e8e0c5197cafee73a6bc17f896d6b214423a7
RyujiShimatani <138374692+RyujiShimatani@users.noreply.github.com>
2023-08-23 10:11:30 +0000

    注文履歴一覧作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,25 @@
 class Admin::OrdersController < ApplicationController
-  
-  def update
+
+  def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
+    @order_details = OrderDetail.where(order_id: @order)
+    @production_status = @order.order_details.pluck(:production_status)
   end
-  
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_details.update_all(production_status: 1) if @order.status == "wait_payment"
+    end
+      redirect_to admin_order_path(@order)
+  end
+
+private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
