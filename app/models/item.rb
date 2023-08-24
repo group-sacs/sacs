d3c68bc4077545fdcc56ee38c0e1c5446cb3143c
chimu-a <138343606+chimu-a@users.noreply.github.com>
2023-08-24 04:41:17 +0000

    [編集中](会員)マイページ(管理者)商品一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
  belongs_to :genre
+ # 下記追記した
  has_many :cart_items
+ # 下記追記した
+ has_many :order_details
   with_options presence: true do
    validates :name
    validates :explamatory_text
    validates :without_tax_price
+   validates :genre_id
    validates :image
  end
  has_one_attached :image
